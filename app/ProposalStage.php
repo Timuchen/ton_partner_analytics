e4b19408b57c90779fe6e3f7f7ccc99138cf63d0
Timuchen <ttkacheff@gmail.com>
2021-07-09 08:33:23 +0000

    start

---
+++ app/ProposalStage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProposalStage extends Model
+{
+    use HasFactory;
+}
